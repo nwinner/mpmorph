aaad309588dd0e255965f8785bbbb185f48916b9
nwinner <nwinner@berkeley.edu>
2019-06-12 12:13:35 -0700

    New modules that I'm working on for this package.
    utils.py:
    Will hold commonly used functions that are used in various MD analysis. Ex: autocorrelation functions, reading data, FFT manipulation, etc. These are still a work in progress, but some of them are working and I've incorporated into diffusion.py and the new vibrations.py
    
    vibrations.py:
    Will be a new module for dealing with vibrational properties (vdos, ir spectrum, raman spectrum) gotten by Green-Kubo formalism. Currently, VDOS works reasonably well while Vibration itself (to be renamed) requires a dipoles.dat file that is hard to get from VASP. Works pretty well if you can get that kind of file.

---
+++ mpmorph/analysis/utils.py
@@ -0,0 +1,280 @@
+from __future__ import division, unicode_literals
+
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+from scipy.signal import savgol_filter as savgol
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import signal
+import math, os, sys, time
+
+from pymatgen.io.lammps.outputs import parse_lammps_dumps
+from pymatgen.io.vasp.outputs import Xdatcar
+
+__author__ = "Nicholas Winner"
+__copyright__ = "None"
+__version__ = "0.2"
+__maintainer__ = "Nicholas Winner"
+__email__ = "nwinner@berkeley.edu"
+__status__ = "Development"
+__date__ = ""
+
+
+def power_spectrum(v, window_func='Gaussian'):
+
+    """
+    This function calculates the power spectral density for 1D array, v. It is very similar to the autocorrelation
+    function, which is the more common function to use. The only difference in these is that there is no final inverse
+    fast Fourier transform in the calculation, and that this function only supports the FFT method, and no "exact"
+    method.
+
+    :param v:
+    :return:
+    """
+
+    if window_func == 'Gaussian':
+        sigma = 2 * math.sqrt(2 * math.log(2))
+        window = signal.gaussian(len(v), std=4000.0 / sigma, sym=False)
+    elif window_func == 'BH':
+        window = signal.blackmanharris(len(v), sym=False)
+    elif window_func == 'Hamming':
+        window = signal.hamming(len(v), sym=False)
+    elif window_func == 'Hann':
+        window = signal.hann(len(v), sym=False)
+
+    WE = sum(window) / len(v)
+    wf = window / WE
+    # convolve the window function.
+    if isinstance(v[0], (list, np.ndarray, tuple)):
+        sig = v * wf[None, :].T
+    else:
+        sig = v * wf.T
+
+    # A series of number of zeros will be padded to the end of the DACF \
+    # array before FFT.
+    N = zero_padding(sig)
+
+    yfft = np.fft.fft(sig, N, axis=0) / len(sig)
+
+    if window_func == 'None':
+        yfft = np.fft.fft(v, n=int(N), axis=0) / len(v)
+        return np.square(np.absolute(yfft))
+    else:
+        return np.square(np.absolute(yfft))
+
+
+def dump_to_df(filename, write_csv=True, output="data.csv"):
+    """
+    A helper function that takes a lammps dump file and returns a Pandas Dataframe. It is also recommended to write
+    a CSV file. This has the benefits:
+        (1) CSV files take up less space than dump files.
+        (2) It is far more efficient to parse massive amounts of lammps data entirely through Pandas instead of
+            using a list of Pandas DataFrames, as Pymatgen currently provides, it just requires a little more knowledge
+            of how to use Pandas efficiently. A single CSV can be read as a DF and then processed.
+        (3) We can pre-sort the particles. LAMMPS does not retain the order of its particles in the dump file, which
+            can be very annoying for post processing. When the csv is written, it sorts the Pd dataframe so that at
+            each time step, the particles are listed in order of their id.
+
+    Args:
+        filename: (str) file name of the lammps dump.
+        write_csv: (bool) Whether or not to write csv file
+        output: (str) file name to output the csv. Include ".csv" in your filename.
+
+    Returns:
+        Pandas Dataframe of the dump
+    """
+
+    dump = parse_lammps_dumps(filename)
+    dfs  = []
+    for frame in dump:
+        dfs.append(frame.data)
+        dfs[-1]['Timestep'] = frame.timestep
+    df = pd.concat(dfs).sort_values(by=['Timestep', 'id']).reset_index(drop=True)
+
+    if write_csv:
+        df.to_csv(output)
+
+    return df
+
+
+def xdatcar_to_df(xdat="XDATCAR"):
+
+    if isinstance(xdat, str):
+        structures = Xdatcar(xdat).structures
+    else:
+        structures = xdat
+    dfs = []
+
+    for i, s in enumerate(structures):
+        coords = [site.coords for site in s.sites]
+        x = [c[0] for c in coords]
+        y = [c[0] for c in coords]
+        z = [c[0] for c in coords]
+        t = [site.specie for site in s.sites]
+        ids = [i+1 for i in range(len(s.sites))]
+
+        dfs.append(pd.DataFrame.from_dict({'x':x,'y':y,'z':z,'type':t,'Id':ids}))
+        dfs[-1]['Timestep'] = i+1
+
+    df = pd.concat(dfs)
+
+    return df
+
+
+def _xdatcar_to_df(filename="XDATCAR", write_csv=True, output="xdatcar.csv"):
+    """
+    Parses a DATCAR file into a Pandas Dataframe, and by default writes a csv file, which is quicker to read when
+    doing further analysis. By default, it is equipped for reading XDATCAR, but the format is general. So, if you
+    have a VDATCAR for velocities for example. You could read that as well.
+
+    Args:
+        filename: (str) Name of the file to read.
+        header: [list] The values that are going to be read from the columns of the DATCAR file. Default='XDATCAR'
+        write_csv: (bool) Whether or not to write the pandas dataframe to a csv file. Defaults to True.
+        output: (str) Name of the csv file to write. Default='xdatcar.csv'
+    Returns:
+        Pandas Dataframe of the datcar
+    """
+
+    f = open(filename)
+    lines = f.readlines()
+    f.close()
+
+    spec = lines[5].split()
+    num  = lines[6].split()
+    num = list(map(int, num))
+    N    = 0
+
+    species = []
+    for i in range(len(num)):
+        for j in range(num[i]):
+            species.append(spec[i])
+
+    N = sum(num) # Number of atoms/ions
+
+    dfs = []
+    j = 8
+    while j+N+8 < len(lines):
+        #temp = [ [float(val) for val in lines[i].split()] for i in range(j, j+N)]
+        temp = [lines[i].split() for i in range(j, j + N)]
+        dfs.append(pd.DataFrame().from_records(temp))
+        dfs[-1]['Timestep'] = (lines[j-1].split()[-1])
+        dfs[-1]['type']   = species
+        dfs[-1]['Id'] = np.arange(1, len(dfs[-1].index)+1)
+        j += N+8
+
+    df = pd.concat(dfs).rename(columns={0: 'x', 1: 'y', 2: 'z'})
+
+    if write_csv:
+        df.to_csv(output)
+
+    return df
+
+
+def ensemble_average(df, function, values, types):
+    """
+    For doing ensemble averaging. Using some function, it will get the ensemble average for all time, and all particle
+    types that you specify
+
+    Args:
+        df: (Dataframe) Input data, needs to have the standard form with 'Id', 'type', and 'Timestep'
+        function: (function) the function to whch we want to pass the ensemble data. I.e. an averaging
+                    function, an autocorrelation function, a spectral density function, etc.
+        values: The data that you want to evaluate with your function.
+                Example: ['x', 'y', 'z']
+        types: [array] Which particle types you want to evaluate.
+                Example: ['H', 'O']
+    :return:
+    """
+    ids = {}
+    for i_type in types:
+        ids[i_type] = df[(df.Timestep == 1) & (df.type == i_type)]['Id'].values
+
+    results = []
+    for key, value in ids.items():
+        for id in value:
+            data = df[df['Id'] == id][values].values
+            results.append(function(data))
+    return np.mean(results, axis=1)
+
+
+def read_data(path, fname):
+    with open(os.path.join(path, fname), "r") as fo:
+        dipole = np.loadtxt(fo, dtype=np.float64, usecols=(1, 2, 3))
+    return dipole
+
+
+def calc_derivative(v, delta_t, thr=0.1):
+    dy = np.zeros(np.shape(v))
+    for i in range(3):
+        dy[:, i] = np.gradient(v[:, i], edge_order=2)
+    dy = dy[~(np.absolute(dy) > thr).any(1), :]
+    return np.divide(dy, delta_t)
+
+
+def zero_padding(sample_data):
+    '''
+      A series of Zeros will be padded to the end of the dipole moment array
+    (before FFT performed), in order to obtain a array with the length which
+    is the "next power of two" of numbers.
+    #### Next power of two is calculated as: 2**np.ceil(log2(x))
+    #### or Nfft = 2**int(math.log(len(data_array)*2-1, 2))
+    '''
+    N = 2 ** int(math.log(len(sample_data) * 2 - 1, 2))
+    return N
+
+
+def autocorrelation(v):
+    # normalization
+    yunbiased = v - np.mean(v, axis=0)
+    ynorm = np.sum(np.power(yunbiased, 2), axis=0)
+    autocor = np.zeros(np.shape(v))
+
+    if isinstance(v[0], (list, tuple, np.ndarray)):
+        for i in range(len(v[0])):
+            autocor[:, i] = signal.fftconvolve(v[:, i], v[:, i][::-1], mode='full')[len(v) - 1:] / ynorm[i]
+    else:
+        autocor = signal.fftconvolve(v, v[::-1], mode='full')[len(v) - 1:] / ynorm
+    return autocor
+
+
+######## Save The Results to A TEXT File ########
+def save_results(fout, wavenumber, intensity):
+    title = ("Wavenumber", "IR Intensity", "cm^-1", "a.u.")
+    with open(fout, "w") as fw:
+        np.savetxt(fout, np.c_[wavenumber[0:5000], intensity[0:5000]],
+                   fmt="%10.5f %15.5e",
+                   header="{0:>10}{1:>16}\n{2:^11}{3:^20}".format(*title),
+                   comments='')
+
+
+######## Plot The Spectrum by Using Matplotlib module ########
+def visualization(D_p, DACF, wavenumber, intensity):
+    plt.subplot(3, 1, 1)
+    L1 = np.arange(len(D_p))
+    plt.plot(L1, D_p[:, 0], color='red', linewidth=1.5)
+    plt.plot(L1, D_p[:, 1], color='green', linewidth=1.5)
+    plt.plot(L1, D_p[:, 2], color='blue', linewidth=1.5)
+    plt.axis([0, len(D_p), 1.1 * np.min(D_p), 1.1 * np.max(D_p)], fontsize=15)
+    plt.xlabel("Data Points", fontsize=15)
+    plt.ylabel("Derivative of Dipole (a.u.)", fontsize=15)
+
+    plt.subplot(3, 1, 2)
+    L2 = np.arange(len(DACF))
+    plt.plot(L2, DACF[:, 0], color='red', linewidth=1.5)
+    plt.plot(L2, DACF[:, 1], color='green', linewidth=1.5)
+    plt.plot(L2, DACF[:, 2], color='blue', linewidth=1.5)
+    plt.axis([0, len(DACF), 1.1 * np.min(DACF), 1.1 * np.max(DACF)], fontsize=15)
+    plt.xlabel("Data Points", fontsize=15)
+    plt.ylabel("DACF (a.u.)", fontsize=15)
+
+    plt.subplot(3, 1, 3)
+    plt.plot(wavenumber, intensity, color='black', linewidth=1.5)
+    plt.axis([0, 4000,
+              -1.1 * np.min(intensity), 1.1 * np.max(intensity)],
+             fontsize=15)
+    plt.xlabel("Wavenumber (cm$^{-1}$)", fontsize=15)
+    plt.ylabel("Intensity (a.u.)", fontsize=15)
+    plt.subplots_adjust(hspace=0.5)
+    plt.show()
---
+++ mpmorph/analysis/vibrations.py
@@ -0,0 +1,194 @@
+from __future__ import division, unicode_literals
+
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+from pymatgen.core.periodic_table import Element
+
+__author__ = "Nicholas Winner"
+__copyright__ = "None"
+__version__ = "0.2"
+__maintainer__ = "Nicholas Winner"
+__email__ = "nwinner@berkeley.edu"
+__status__ = "Development"
+__date__ = ""
+
+from mpmorph.analysis.utils import autocorrelation, power_spectrum, calc_derivative, xdatcar_to_df
+
+boltzmann = 8.6173303e-5       # eV/K
+plank     = 4.135667e-15       # eV/s
+plank_ps  = plank / 1e12       # eV/ps
+hbar      = plank/(2*np.pi)    # eV/s
+hbar_ps   = plank_ps/(2*np.pi) # eV/ps
+
+c     = 2.9979245899e10  # speed of light in vacuum in [cm/s], from Wikipedia.
+kB    = 0.6950347  # Boltzman constant in [cm^-1/K], from Wikipedia.
+h_bar = 6.283185  # Reduced Planck constant in atomic unit, where h = 2*pi
+
+class Vibrations(object):
+
+    def __init__(self, data):
+        self.data = data
+        self.vdos_spectrum  = None
+        self.ir_spectrum    = None
+        self.raman_spectrum = None
+
+        self.D_p = None
+        self.DACF = None
+
+    def calc_vdos_spectrum(self, filename=None):
+
+        df = pd.read_csv("/Users/nwinner/bin/venv/xdatcar.csv")
+
+        def vel(x):
+            return np.gradient(x, axis=0)
+
+        v_li = vel(df[df['Id'] == 1][['x', 'y', 'z']])
+        v_f = vel(df[df['Id'] == 2][['x', 'y', 'z']])
+        v_be = vel(df[df['Id'] == 3][['x', 'y', 'z']])
+
+        fig, axs = plt.subplots(2)
+
+        axs[0].plot(autocorrelation(v_li), label='Li')
+        axs[0].plot(autocorrelation(v_f), label='F')
+        axs[0].plot(autocorrelation(v_be), label='Be')
+
+        print(autocorrelation(v_li))
+
+        axs[1].plot(power_spectrum(autocorrelation(v_li))[0:200], label='Li')
+        axs[1].plot(power_spectrum(autocorrelation(v_f))[0:200], label='F')
+        axs[1].plot(power_spectrum(autocorrelation(v_be))[0:200], label='Be')
+        plt.legend()
+        plt.show()
+
+    def calc_ir_spectrum(self, delta_t=1e-15):
+        """
+        Calculate the IR spectrum from the dipole autocorrelation function.
+
+            P(freq) = m * freq**2 * FFT( ACF(x) )
+            P(freq) = m * FFT( ACF(derivative(x)) )
+
+        :return:
+        """
+
+        data = self.data[['dipole_x', 'dipole_y', 'dipole_z']]
+
+        D_p  = calc_derivative(data.to_numpy(), delta_t=delta_t)  # derivative of dipole
+        DACF = autocorrelation(D_p)                    # Dipole derivative autocorrelation function
+        yfft = power_spectrum(DACF)                    # power spectrum of the dipole derivative ACF
+
+        wavenumber = np.fft.fftfreq(len(yfft), delta_t * c)[0:int(len(yfft) / 2)]  # Wavenumber in units of cm^-1
+        prefactor = 1
+        intensity = prefactor*np.sum(yfft, axis=1)[0:int(len(yfft) / 2)]
+        intensity = intensity/np.max(intensity)
+
+        # TODO: Create pre-factor
+
+        self.D_p = D_p
+        self.DACF = DACF
+        self.ir_spectrum = pd.DataFrame.from_dict({'wavenumber': wavenumber, 'intensity': intensity})
+
+    def calc_raman_spectrum(self):
+        return
+
+    def calc_spectra(self):
+
+        self.calc_power_spectrum()
+        self.calc_ir_spectrum()
+        self.calc_raman_spectrum()
+
+    def plot_ir(self):
+
+        D_p = self.D_p
+        DACF = self.DACF
+        wavenumber = self.ir_spectrum['wavenumber']
+        intensity = self.ir_spectrum['intensity']
+
+        fig, axs = plt.subplots(2)
+
+        L2 = np.arange(len(DACF))
+        axs[0].plot(L2, DACF[:, 0], color='red', linewidth=1.5)
+        axs[0].plot(L2, DACF[:, 1], color='green', linewidth=1.5)
+        axs[0].plot(L2, DACF[:, 2], color='blue', linewidth=1.5)
+        axs[0].axis([0, len(DACF), 1.1 * np.min(DACF), 1.1 * np.max(DACF)], fontsize=15)
+        axs[0].set_xlabel("Data Points", fontsize=15)
+        axs[0].set_ylabel("DACF (a.u.)", fontsize=15)
+
+        axs[1].plot(wavenumber, intensity, color='black', linewidth=1.5)
+        axs[1].axis([0, 4000,
+                  -1.1 * np.min(intensity), 1.1 * np.max(intensity)],
+                 fontsize=15)
+        axs[1].set_xlabel("Wavenumber (cm$^{-1}$)", fontsize=15)
+        axs[1].set_ylabel("Intensity (a.u.)", fontsize=15)
+        plt.show()
+
+    def to_csv(self):
+        return
+
+class VDOS(object):
+
+    def __init__(self, filename):
+        if isinstance(filename, str):
+            if '.csv' in filename:
+                self.positions = pd.read_csv(filename)
+        else:
+            self.positions = xdatcar_to_df(filename)
+
+        self.vel  = {}
+        self.acfs = {}
+        self.vdos = {}
+        self.freq = {}
+
+    def calc_vdos_spectrum(self, time_step=1):
+
+        df = self.positions
+
+        def vel(x):
+            return np.divide(np.gradient(x, axis=0), time_step)
+
+        ids = {}
+        types = df.type.unique()
+        for i_type in types:
+            ids[i_type] = df[(df.Timestep == 1) & (df.type == i_type)]['Id'].values
+
+        for key, value in ids.items():
+            results = []
+            for id in value:
+                data = df[df['Id'] == id][['x', 'y', 'z']]
+                results.append(np.mean(vel(data), axis=1))
+            self.vel[key] = results
+
+        for key, value in self.vel.items():
+            acf = []
+            for v in value:
+                acf.append(autocorrelation(v))
+            mean_acf = np.mean(acf, axis=0)
+            self.acfs[key] = mean_acf
+
+        for key, value in self.acfs.items():
+            spectrum  = power_spectrum(value)*Element(key).atomic_mass
+            intensity = spectrum / np.max(spectrum)
+            self.freq[key] = np.fft.fftfreq(len(spectrum), time_step)[0:int(len(spectrum) /2)]  # freq
+            self.vdos[key] = intensity[0:int(len(intensity)/2)]
+
+    def plot_vdos(self, show=True, save=False):
+        fig, axs = plt.subplots(2)
+
+        for k,v in self.acfs.items():
+            axs[0].plot(v, label=k)
+        axs[0].set_xlabel('Steps', size=22)
+        axs[0].set_ylabel('Velocity Autocorrelation Function, VACF', size=14)
+
+        for k,v in self.vdos.items():
+            p = axs[1].plot(self.freq[k], v, label=k)
+            axs[1].fill(self.freq[k], v, alpha=0.3, color=p[-1].get_color())
+        axs[1].set_xlabel('Frequency ($fs^{-1}$)', size=22)
+        axs[1].set_xscale('log')
+        axs[1].set_ylabel('Velocity Density of States (a.u.)', size=14)
+
+        plt.legend()
+        if save:
+            plt.savefig('vdos.png', fmt='png')
+        if show:
+            plt.show()
+        return plt
